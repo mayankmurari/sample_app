5dd759d53923260f8940d6e498fca904e1ee8fec
Mayank Murari <mayank.murari@gmail.com>
2014-03-28 12:26:34 +0530

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ SampleApp::Application.routes.draw do
   # get "static_pages/contact"
   match '/contact', to: 'static_pages#contact', via: 'get'
   # get "users/new"
-  match '/users', to: 'users#new', via: 'get'
+  match '/users', to: 'users#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :users
