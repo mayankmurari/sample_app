007634a2776de4da19f6481bb03c63f2cb2b0b6e
Mayank Murari <mayank.murari@gmail.com>
2014-03-29 15:20:58 +0530

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,12 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
   belongs_to :user
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ SampleApp::Application.routes.draw do
   match '/users', to: 'users#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   resources :sessions, only: [:new, :create, :destroy]
   match '/signup', to: 'users#new', via: 'get'
@@ -20,6 +24,7 @@ SampleApp::Application.routes.draw do
   match '/signout', to: 'sessions#destroy', via: 'delete'
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
